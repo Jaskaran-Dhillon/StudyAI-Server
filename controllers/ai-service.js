eb75a768ab16e400ddaf28cb529d02f1262859c9
Jaskaran <jas.dhillon@hotmail.com>
2024-03-09 21:23:48 -0500

    PDF Write working, according to old script. Modified script to accept params.

--- controllers/ai-service.js
+++ controllers/ai-service.js
@@ -3,91 +3,127 @@ const User = require("../models/user");
 const { isBlank } = require("../util/helper");
 const { trim, escape } = require("validator");
 const spawn = require("child_process").spawn;
-const fs = require('fs');
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
 
-const sanitizeRequest = (request) => {
-  let { action, fileType, file, instruction } = request;
-  const sanitizedUser = {
-    action: escape(trim(action || "")),
-    fileType: escape(trim(fileType|| "")),
-    file: escape(trim(file || "")),
-    instruction: escape(trim(instruction || "")),
-  };
+// const sanitizeRequest = (request) => {
+//   let { action, fileType, file, instruction } = request;
+//   const sanitizedUser = {
+//     action: escape(trim(action || "")),
+//     fileType: escape(trim(fileType || "")),
+//     file: escape(trim(file || "")),
+//     instruction: escape(trim(instruction || "")),
+//   };
 
-  return sanitizedUser;
-};
-// action = create overall summary or summarize the input into notes
-// extension = pdf, text document, transcript (vtt)
-// file = data
-// instruction = inst to append onto the python script
+//   return sanitizedUser;
+// };
 
 exports.summarize = async (req, res) => {
-  // console.log(req);
   // console.log(req.body);
-  console.log(req.file);
+  // console.log(req.file);
+  let body = req.body;
+  await fs.readFile(req.file.path, function (err1, data) {
+    if (err1) throw err;
 
-  await fs.readFile(req.file.path, function(err1, data){
-
-    if(err1) throw err;
-
-      fs.writeFile("./dataset/" + req.file.originalname, data, function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    }); 
+    fs.writeFile("./dataset/" + req.file.originalname, data, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("The file was saved!");
+    });
   });
   // const body = sanitizeRequest(req.body);
   // console.log(body);
-  //check what kind of request it is
 
-  //in correct section, call the script, pass in the input file, extension, and instruction as needed
-  
-  let fileType = "pdf";
-  let verbosity = 0;
-  let summary = "true";
-  let bullet = "true";
-  let keyWord = "true";
-  // const ls = spawn("python", ["./scripts/test_ai_pdf.py", req.file.originalname, fileType, verbosity, summary, bullet, keyWord ]);
+  let fileType = body.fileType;
+  let verbosity = Number.parseInt(body.verbosity);
+  let summary = body.summary;
+  let bullet = body.bullet;
+  let keyWord = body.keyWord;
 
-  // let output = {};
-  // let bufferArray = [];
-  // ls.stdout.on("data", (data) => {
-  //   // output = data;
-  //   // console.log(`stdout: ${data}`);
-  //   // console.log(data.stdout);
-  //   // console.log(data.title)
-  //   // console.log(`output: ${output}`)
-  //   // console.log(typeof data);
-  //   // console.log(data.toString())
-  //   // console.log(eval(data.toString()))
-  //   bufferArray.push(data);
-  //   // res.status(200).json({
-  //   //   status: "OK",
-  //   //   result: JSON.stringify(data)
-  //   //  });
-  // });
+  const ls = spawn("python", [
+    "./scripts/main.py",
+    req.file.originalname,
+    fileType,
+    verbosity,
+    summary,
+    bullet,
+    keyWord,
+  ]);
 
-  
+  let bufferArray = [];
 
-  // ls.stderr.on("data", (data) => {
-  //   console.log(`stderr: ${data}`);
-  // });
-
-  // ls.on("close", (code) => {
-  //   console.log(`child process exited with code ${code}`);
-  //   let dataBuffer = Buffer.concat(bufferArray);
-  //   console.log(JSON.parse(dataBuffer.toString()))
-  //   res.status(200).json({
-  //     status: "OK",
-  //     data: dataBuffer.toString()
-  //    });
-  // });
-
-  res.status(200).json({
-      status: "OK",
-      data: req.file
+  ls.stdout.on("data", (data) => {
+    bufferArray.push(data);
   });
+
+  ls.stderr.on("data", (data) => {
+    console.log(`stderr: ${data}`);
+  });
+
+  ls.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+    let dataBuffer = Buffer.concat(bufferArray);
+
+    let result = JSON.parse(dataBuffer.toString());
+    console.log(result);
+
+    // Create a document
+    const doc = new PDFDocument();
+
+    doc.pipe(fs.createWriteStream("output.pdf"));
+
+    // Formatting the PDF
+    doc.font("Times-Bold").fontSize(20).text(result.title, {
+      align: "center",
+    });
+
+    if (summary) {
+      doc.moveDown();
+      doc.font("Times-Roman").fontSize(15).text("Summary:", {
+        underline: true,
+      });
+      doc.font("Times-Roman").fontSize(15).text(result.summary);
+    }
+
+    if (bullet) {
+      doc.moveDown();
+      doc.font("Times-Roman").fontSize(15).text("Key notes:", {
+        underline: true,
+      });
+      doc.font("Times-Roman").fontSize(15).list(result.main_points);
+    }
+
+    // if (keyWord) {
+    // doc.moveDown();
+    //   doc.font("Times-Roman").fontSize(15).text("Key words:", {
+    //     underline: true,
+    //   });
+    //   doc.font("Times-Roman").fontSize(15).text(result.key_words);
+    // }
+
+    // Finalize PDF file
+    doc.end();
+    console.log("PDF Created");
+
+    var file = fs.createReadStream("output.pdf");
+    var stat = fs.statSync("output.pdf");
+    res.setHeader("Content-Length", stat.size);
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", "attachment; filename=output.pdf");
+    file.pipe(res);
+    // res.status(200).json({
+    //   status: "OK",
+    //   data: dataBuffer.toString(),
+    // });
+
+    //TODO: delete input and output file here
+  });
+
+  // res.status(200).json({
+  //     status: "OK",
+  //     data: req.file
+  // });
   //once summarized, store the file into the files table (or just store it in local storage if out of time)
 
   //return file with 200
--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,22 @@
 from studyai_notes import Studyai_Notes
 from prompt import Prompt
-
+import sys
+# ard 1 = filename, arg2 = filetype, arg 3 = verbosity, arg4 = summary, arg5 = bullet, arg6 = keyWord
 
 def main():
+    # prompt_creator = Prompt(
+    #     verbosity=0,
+    #     summary=True,
+    #     bullet=True,
+    #     key_word=False,
+    #     equation=False,
+    #     example=False,
+    # )
     prompt_creator = Prompt(
-        verbosity=0,
-        summary=True,
-        bullet=True,
-        key_word=False,
+        verbosity=sys.argv[3],
+        summary=True if (sys.argv[4] == "true") else False,
+        bullet=True if (sys.argv[5] == "true") else False,
+        key_word=True if (sys.argv[6] == "true") else False,
         equation=False,
         example=False,
     )
@@ -15,8 +24,9 @@ def main():
     system_prompt = prompt_creator.create_prompt()
     print(system_prompt)
 
-    study = Studyai_Notes("data/League_Rules_Guidelines.pdf", "pdf")
-
+    # study = Studyai_Notes("data/League_Rules_Guidelines.pdf", "pdf")
+    study = Studyai_Notes("./dataset/" + sys.argv[1], sys.argv[2])
+    
     # print(study.get_context())
 
     notes = study.note_taker(system_prompt)
